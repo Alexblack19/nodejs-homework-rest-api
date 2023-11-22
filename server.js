ef2b5ee92d54015ff867cc9f450ceb02ad0342db
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-11-22 23:01:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,3 @@ const app = require("./app");
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000");
 });
-
-// ==========================================
-// const contacts = require("./models/contacts");
-// async function invokeAction() {  
-//       const allContacts = await contacts.listContacts();
-//       return console.table(allContacts);
-// }
-// invokeAction()
