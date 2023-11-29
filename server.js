1320c5e443f0db4a64b7ff2ce01bf871b743ff89
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-11-29 15:48:58 +0200

    update

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+const mongoose = require("mongoose");
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://dragondaewoo:9YgN6ud1YnviLGM2@cluster0.mlxsxs9.mongodb.net/db-contacts";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
