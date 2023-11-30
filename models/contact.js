4aa251b79e78204b23ef4556f1566ca91c6c733c
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-11-30 20:59:05 +0200

    update

--- models/contact.js
+++ models/contact.js
@@ -1,22 +1,53 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require("mongoose"); // Mongoose перевіряє дані, які зберігаємо в базі даних. Перевіряє те, що ми хочемо зберегти
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const Joi = require("joi"); // Joi перевіряє дані, які приходять з бази даних. Перевіряє те що прийшло
+
+const { handleMongooseError } = require("../helpers");
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
+  { versionKey: false, timestamps: true },
+);
+
+const addSchema = Joi.object({
+  name: Joi.string().required().messages({
+    "any.required": `missing required name field`,
+  }),
+  email: Joi.string().required().messages({
+    "any.required": `missing required email field`,
+  }),
+  phone: Joi.string().required().messages({
+    "any.required": `missing required phone field`,
+  }),
+  favorite: Joi.boolean(),
 });
+
+contactSchema.post("save", handleMongooseError);
 
 const Contact = model("contact", contactSchema);
 
-module.exports = Contact;
+const schemas = {
+  addSchema,
+};
+
+module.exports = {
+  Contact,
+  schemas,
+};
