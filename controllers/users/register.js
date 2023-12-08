1a54ddddf84347365bcef9de5d36d2fc12c343de
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-12-08 20:42:55 +0200

    Update register.js

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,15 +1,21 @@
+const bcryptjs = require("bcryptjs");
+
 const { HttpError } = require("../../helpers");
+
 const User = require("../../models/user");
 
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (user) {
     throw HttpError(409, "Email already in use");
   }
 
-  const newUser = await User.create(req.body);
+  const hashPassword = await bcryptjs.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashPassword });
+
   res.status(201).json({
     user: { email: newUser.email, subscription: newUser.subscription },
   });
