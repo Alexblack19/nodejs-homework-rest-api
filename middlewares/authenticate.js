742462ea3f5a74cf72d278627829372669f43da2
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-12-09 22:32:26 +0200

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,10 +1,27 @@
-const authenticate = async (req, res, next) => {
-  // const { authorization = "" } = req.headers;
-  console.log("authenticate");
+const jwt = require("jsonwebtoken");
 
-  // const [bearer, token] = authorization.split(" ");
-  // console.log(bearer);
-  // console.log(token);
+const { HttpError } = require("../helpers");
+const User = require("../models/user");
+
+const { SECRET_KEY } = process.env;
+
+const authenticate = async (req, res, next) => {
+  const { authorization = "" } = req.headers;
+  const [bearer, token] = authorization.split(" ");
+  if (bearer !== "Bearer") {
+    next(HttpError(401, "Not authorized"));
+  }
+
+  try {
+    const { id } = jwt.verify(token, SECRET_KEY);
+    const user = await User.findById(id);
+    if (!user) {
+      next(HttpError(401, "There is no user in the database"));
+    }
+    next();
+  } catch {
+    next(HttpError(401, "Not authorized"));
+  }
 };
 
 module.exports = authenticate;
