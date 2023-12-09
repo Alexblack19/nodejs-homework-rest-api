dfbe66ac9e920da58946e7dde075f9ece96054b9
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-12-09 09:37:56 +0200

    update

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,9 +1,10 @@
 const authenticate = async (req, res, next) => {
-  const { authorization = "" } = req.headers;
-  console.log(authorization);
-  const [bearer, token] = authorization.split(" ");
-  console.log(bearer);
-  console.log(token);
+  // const { authorization = "" } = req.headers;
+  console.log("authenticate");
+
+  // const [bearer, token] = authorization.split(" ");
+  // console.log(bearer);
+  // console.log(token);
 };
 
 module.exports = authenticate;
