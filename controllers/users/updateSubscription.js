89601f961357fb71b5323b7dbe3693f97a454b0e
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-12-11 01:08:36 +0200

    update

--- controllers/users/updateSubscription.js
+++ controllers/users/updateSubscription.js
@@ -1,5 +1,12 @@
-const updateSubscription = (req, res)=>{
+const User = require("../../models/user");
 
-}
+const updateSubscription = async (req, res) => {
+  const { _id } = req.user;
+  //   const {subscription} = req.body;
+  // console.log(req.body)
+
+  const result = await User.findByIdAndUpdate(_id, req.body, { new: true });
+  res.json(result);
+};
 
 module.exports = updateSubscription;
