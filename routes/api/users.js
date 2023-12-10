8820b7ee860db3e970cda7dcb78ff869e0125fdf
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-12-10 23:21:50 +0200

    update

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { validateBody } = require("../../middlewares");
+const { validateBody, authenticate } = require("../../middlewares");
 
 const { registerSchema, loginSchema } = require("../../schemas/user");
 
@@ -13,5 +13,9 @@ router.post("/register", validateBody(registerSchema), ctrl.register);
 
 // signin
 router.post("/login", validateBody(loginSchema), ctrl.login);
+
+router.get("/current", authenticate, ctrl.getCurrent);
+
+router.post("/logout", authenticate, ctrl.logout);
 
 module.exports = router;
