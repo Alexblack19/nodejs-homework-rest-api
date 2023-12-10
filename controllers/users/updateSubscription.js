1816e9596eb7b7511c275928da99e240769d69ea
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-12-11 01:25:20 +0200

    Update updateSubscription.js

--- controllers/users/updateSubscription.js
+++ controllers/users/updateSubscription.js
@@ -2,10 +2,11 @@ const User = require("../../models/user");
 
 const updateSubscription = async (req, res) => {
   const { _id } = req.user;
-  //   const {subscription} = req.body;
-  // console.log(req.body)
-
-  const result = await User.findByIdAndUpdate(_id, req.body, { new: true });
+  const { subscription } = req.body;
+  // Треба додати перевірку
+  //
+  //
+  const result = await User.findByIdAndUpdate(_id, { subscription }, { new: true });
   res.json(result);
 };
 
