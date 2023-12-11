51a12f18a2bb8d11d6cccf35aaae0ff2e09b3c51
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-12-11 18:32:35 +0200

    update

--- controllers/users/logout.js
+++ controllers/users/logout.js
@@ -1,7 +1,6 @@
 const User = require("../../models/user");
 
 const logout = async (req, res) => {
-console.log(req.user)
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: "" });
   res.status(204).json();
