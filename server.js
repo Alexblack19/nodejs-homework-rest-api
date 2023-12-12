5ddf8f6ea81765cdd97e2a6d756eb390c2556ff6
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-12-12 23:19:58 +0200

    update

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
+require("@colors/colors");
+
 const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(PORT);
-    console.log("Database connection successful");
+    console.log("Database connection successful".blue.italic.bold.underline);
   })
   .catch(error => {
     console.log(error.message);
