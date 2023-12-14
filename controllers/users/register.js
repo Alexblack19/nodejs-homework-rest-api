0042c92e9cd0c48c6e924ea6ed3d41dccc2264f7
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-12-15 00:38:05 +0200

    update

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -4,6 +4,8 @@ const { HttpError } = require("../../helpers");
 
 const User = require("../../models/user");
 
+const gravatar = require("gravatar"); // пакет для створення тимчасової аватарки
+
 const register = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
@@ -13,8 +15,9 @@ const register = async (req, res) => {
   }
 
   const hashPassword = await bcryptjs.hash(password, 10);
+  const avatarURL = gravatar.url(email);
 
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
 
   res.status(201).json({
     user: {
