4ecdc633b9a04cc11337392df93aff95379dde1f
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-12-18 17:59:50 +0200

    update

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,20 +1,22 @@
 const path = require("path");
 const fs = require("fs/promises");
 const User = require("../../models/user");
-// const Jimp = require("jimp");
+const jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
-  console.log("originalname", originalname);
   const fileName = `${_id}_${originalname}`;
-  console.log("fileName", fileName);
   const resultUpload = path.join(avatarsDir, fileName);
   await fs.rename(tempUpload, resultUpload);
+
+  const avatar = await jimp.read(resultUpload);
+  avatar.resize(250, 250);
+  avatar.writeAsync(resultUpload);
+
   const avatarURL = path.join("avatars", fileName);
-  console.log("avatarURL", avatarURL);
   await User.findByIdAndUpdate(_id, { avatarURL });
 
   res.json({
